e3267d10e9275977f4a4783961bf01abf21210ae
Kai Gothe <kaigostudio@gmail.com>
2019-03-15 09:45:50 +0100

    Update SeminarGroup.js

--- Prototype Parsing/modules/DB_Modules/SeminarGroup.js
+++ Prototype Parsing/modules/DB_Modules/SeminarGroup.js
@@ -1,16 +1,16 @@
 const db = require("../../dbConnection");
 
 const SeminarGroup = {
-    write:function(name, callback){
-        return db.query("INSERT INTO `Semester` (`fachSemesterID`, `name`, `created`, `updated`, `lastLookup`) VALUES (NULL, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL)", [name], callback);
+    write:function(name, course, semester, callback){
+        return db.query("INSERT INTO `SeminarGroup` (`seminarGroupID`, `name`, `semester`, `course`, `created`, `updated`, `lastLookup`) VALUES (NULL, ?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL)", [name, semester, course], callback);
     },
 
     exist_Name:function(name, callback){
-        return db.query("SELECT fachSemesterID FROM `Semester` WHERE name = ?", [name], callback);
+        return db.query("SELECT seminarGroupID FROM `SeminarGroup` WHERE name = ?", [name], callback);
     },
 
     setSelected:function(rowID, callback){
-        return db.query("UPDATE `Semester` SET `lastLookup`= CURRENT_TIMESTAMP() WHERE fachSemesterID = ?", rowID, callback);
+        return db.query("UPDATE `SeminarGroup` SET `lastLookup`= CURRENT_TIMESTAMP() WHERE seminarGroupID = ?", rowID, callback);
     }
 }
 
