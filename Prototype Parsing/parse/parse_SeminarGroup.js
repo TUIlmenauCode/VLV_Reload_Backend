75911483acb6290f3b34d8d2c0b8c9a87fd194d7
Kai Gothe <kaigostudio@gmail.com>
2019-03-15 09:52:47 +0100

    Update parse_SeminarGroup.js

---
+++ Prototype Parsing/parse/parse_SeminarGroup.js
@@ -0,0 +1,24 @@
+const DB_parse_index = require("../modules/DB_Modules/parsed_index");
+const DB_Semester = require("../modules/DB_Modules/Semester");
+const utility = require("../modules/utility");
+const log = require("../modules/Log");
+
+const Parse = {
+    start:function(callback){
+        console.log("--> Start parsing Seminargroups");
+     
+        // bekomme Course id
+
+        // bekomme FS id
+
+        // bekomme URL 
+
+        // parse Select option 
+
+        // eintragen 
+
+
+    }
+}
+
+module.exports = Parse;
